b9f9facb8d3d84d9a8332cca7efc070f282401bc
SER1OGA <53183154+SER1OGA@users.noreply.github.com>
2024-01-04 13:34:23 +0200

    Add files via upload

---
+++ main.rb
@@ -0,0 +1,21 @@
+require_relative 'item'
+require_relative 'cart'
+require_relative 'parser'
+
+url = 'http://books.toscrape.com'
+items = Item.from_url(url)
+
+cart = Cart.new
+
+settings = { max_price: 30.0 }
+
+items.each do |item|
+  parsed_item = Parser.parse_item(item, settings)
+  cart.add_to_cart(parsed_item) if parsed_item
+end
+
+cart.total_price
+
+cart.save_to_file('cart.txt')
+cart.save_to_json('cart.json')
+cart.save_to_csv('cart.csv')
